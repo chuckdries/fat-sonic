9f79ebfc7f48b0ed821a6f00a50c04d5f7513530
Chuck Dries <chuck@chuckdries.com>
2018-09-01 12:33:18 -0700

    Give round character a shot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,9 +26,10 @@ public class PlayerController : MonoBehaviour
     float moveHorizontal = Input.GetAxis("Horizontal");
     // float moveVertical = Input.GetAxis("Vertical");
 
-    // Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
 
-    rb.AddForce(this.transform.right * moveHorizontal * speed);
+    rb.AddForce(movement * speed);
+    // rb.AddForce(this.transform.right * moveHorizontal * speed);
   }
 
   void OnTriggerEnter(Collider other) 
